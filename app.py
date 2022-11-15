425853a82f89c566f2925f291d3d69627056f115
Audrey Maldonado <audrey@render.com>
2022-11-15 12:59:55 -0500

    add an example template

--- app.py
+++ app.py
@@ -104,5 +104,4 @@ from flask import render_template
 @app.route('/hello/<name>')
 
 def hello(name=None):
-    return render_template('hello.html, name=name')
-    
+    return render_template('example.html', name=name)
